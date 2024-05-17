0b3fd5ae86f4701e6dfb57b04b662b17c61b7cde
Malek Kazal <ghazal.malek@gmail.com>
2024-05-16 22:43:03 -0400

    user routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import connectDB from './DB/db.js';
 import chalk from 'chalk';
 import bodyParser from 'body-parser';
 import User from './models/UserModel.js';
+import bcrypt from 'bcrypt';
+
 dotenv.config();
 
 const app = express();
@@ -13,22 +15,82 @@ connectDB();
 
 app.use(bodyParser.json());
 
+// GET All Users
 app.get('/users', async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).send(users);
+    res.status(200).json(users);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
+// GET User by ID
+app.get('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error.' });
+  }
+});
+
+// CREATE New User
 app.post('/user', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
-    res.status(201).send(user);
+    res.status(201).json(user);
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).json({ error: 'Bad request.' });
+  }
+});
+
+// UPDATE User by ID
+app.put('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = {
+      username: req.body.username,
+      email: req.body.email,
+    };
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      updates.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    const user = await User.findByIdAndUpdate(id, updates, { new: true });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error.' });
+  }
+});
+
+// DELETE User by ID
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error.' });
   }
 });
 
